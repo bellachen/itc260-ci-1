f9fedfcc215e3f3db00aa30f8bbeda8ed589ada6
bellachen <bellachen.nyc@gmail.com>
2018-07-27 14:48:05 -0700

    Initial commit

---
+++ models/Pics_model.php
@@ -0,0 +1,27 @@
+<?php
+//application/models/News_model.php
+class Pics_model extends CI_Model {
+
+        public function __construct()
+        {
+            $this->load->database();
+        }
+    
+        public function get_pics($api_key)
+        {
+                $tags = 'bears,polar';
+                $perPage = 25;
+                $url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
+                $url.= '&api_key=' . $api_key;
+                $url.= '&tags=' . $tags;
+                $url.= '&per_page=' . $perPage;
+                $url.= '&format=json';
+                $url.= '&nojsoncallback=1';
+
+                $response = json_decode(file_get_contents($url));
+                $pics = $response->photos->photo;
+            
+                return $pics;
+        }
+    
+}
